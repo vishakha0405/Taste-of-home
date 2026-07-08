7ba689fca7e50c44107515e29f6acbb27c994e5d
vishakha0405 <vishakhabatham19@gmail.com>
2026-07-08 14:00:16 +0530

    Fix deployment issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nmsffydtorqoylxfgxep.supabase.co",
+      },
+    ],
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
